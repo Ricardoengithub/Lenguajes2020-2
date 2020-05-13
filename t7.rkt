bc966c7c935d9d7c0cc2e7c02357e38a3b372ead
Ricardo Ruiz <gaanzz11@gmail.com>
2020-05-13 00:58:55 -0500

    Memoizacion

---
+++ t7.rkt
@@ -0,0 +1,33 @@
+#lang plai
+
+ ( define ( fibo n )
+ ( fibo-memo n ( make-hash ( list ( cons 0 1) ( cons 1 1)))))
+
+ ;; fibo-memo : number hash-table → number
+ ( define ( fibo-memo n tbl )
+ (let ([ busqueda ( hash-ref! tbl n 'vacia )])
+   ( cond [( equal? busqueda 'vacia )
+           ( define nuevo
+            (+ ( fibo-memo (- n 1) tbl ) ( fibo-memo (- n 2) tbl )))
+           ( hash-set! tbl n nuevo ) nuevo]
+
+          [ else busqueda ])))
+
+
+
+( define tbl ( make-hash (list (cons 2 #t) )))
+
+ ( define ( primo? n)
+ (let ([ busqueda ( hash-ref! tbl n 'vacia )])
+   ( cond [( equal? busqueda 'vacia )
+           ( define nuevo
+                   ( aux n 2 ( sub1 n )))
+           ( hash-set! tbl n nuevo ) nuevo]
+
+          [ else busqueda ])))
+
+
+( define ( aux n i j ) ( cond
+                          [( > i j ) #t ]
+                          [( zero? ( modulo n i )) #f ]
+                          [ else ( aux n ( add1 i ) j )]))
